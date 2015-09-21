a4c153bcb347ea0d8323583b1763ed1583d189d0
devonhg <devon.hugh.godfrey@gmail.com>
2015-09-21 00:56:15 -0400

    v1.0.5 modified header to not show on news pages.

--- inc/custom-functions.php
+++ inc/custom-functions.php
@@ -4,10 +4,12 @@
 */
 
 function dhg_title(){
-		$page_id = get_the_ID();
-		$page_title = get_the_title( $page_id ); 
+		if ( is_page() ){
+			$page_id = $post;
+			$page_title = get_the_title( $page_id ); 
 
-		if ( $page_title != "" ){
-			echo "<h1 class='entry-title page-title'> " . $page_title . "</h1>";
+			if ( $page_title != "" ){
+				echo "<h1 class='entry-title page-title'> " . $page_title . "</h1>";
+			}
 		}
 }
